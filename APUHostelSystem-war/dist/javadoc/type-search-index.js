d91cd2837b23f2ceb819061f6b8e01ea0f0f6191
lmoha <lmoha@Luai.realtek>
2025-03-14 16:56:05 +0800

    Removed unnecessary tracked files

--- APUHostelSystem-war/dist/javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"servlet","l":"LoginServlet"},{"p":"servlet","l":"SignupServlet"},{"p":"servlet","l":"UpdateProfileServlet"}]
